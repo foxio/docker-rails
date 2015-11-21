4bf47da3edf4aa1c4dd91f7979f409ac80b416f6
Michael Fisher <mike@fox.io>
2015-11-21 16:07:05 -0500

    Updating Installed Gem Versions

--- install_gems.sh
+++ install_gems.sh
@@ -11,12 +11,12 @@ gem install jbuilder --version "~> 2.0"
 gem install sdoc --version "~> 0.4.0"
 gem install letter_opener_web --version "~> 1.2.0"
 gem install high_voltage --version "~> 2.2.1"
-gem install factory_girl_rails --version "~> 4.0"
-gem install rspec-rails --version "~> 3.0"
+gem install factory_girl_rails --version "~> 4.2"
+gem install rspec-rails --version "~> 3.4"
 gem install web-console --version "~> 2.0"
 gem install mysql2 --version "~> 0.3.20"
 
 # Unversioned Gems
 gem install bundler haml-rails spring kaminari bullet guard guard-rspec guard-teaspoon \
   letter_opener database_cleaner shoulda-matchers json_spec simplecov pg \
-  sqlite3 unicorn rubocop
+  sqlite3 unicorn rubocop meta-tags pry pry-nav timecop
