66cd3b9fdd69304d16654bb60cc8f2f86be52780
Michael Fisher <mike@fox.io>
2015-11-28 07:01:51 -0500

    Updating gem versions to recent versions

--- install_gems.sh
+++ install_gems.sh
@@ -1,22 +1,28 @@
 #!/bin/bash
-gem install rails --version "~> 4.2.0"
+gem install rails --version "~> 4.2.5"
 
 # Versioned Gems
-gem install annotate --version "~> 2.6.5"
-gem install json --version "~> 1.8"
-gem install sass-rails --version "~> 5.0"
-gem install uglifier --version ">= 1.3.0"
-gem install coffee-rails --version "~> 4.1.0"
-gem install jbuilder --version "~> 2.0"
-gem install sdoc --version "~> 0.4.0"
-gem install letter_opener_web --version "~> 1.2.0"
-gem install high_voltage --version "~> 2.2.1"
-gem install factory_girl_rails --version "~> 4.2"
-gem install rspec-rails --version "~> 3.4"
-gem install web-console --version "~> 2.0"
-gem install mysql2 --version "~> 0.3.20"
+gem install annotate --version "~> 2.6.5" --no-rdoc
+gem install json --version "~> 1.8" --no-rdoc
+
+gem install uglifier --version ">= 1.3.0" --no-rdoc
+gem install coffee-rails --version "~> 4.1.0" --no-rdoc
+gem install jbuilder --version "~> 2.0" --no-rdoc
+gem install sdoc --version "~> 0.4.0" --no-rdoc
+gem install high_voltage --version "~> 2.4.0" --no-rdoc
+gem install web-console --version "~> 2.0" --no-rdoc
+
+gem install sass-rails --version "~> 5.0" --no-rdoc
+gem install factory_girl_rails --version "~> 4.5.0" --no-rdoc
+gem install rspec-rails --version "~> 3.4.0" --no-rdoc
+
+gem install nokogiri --version "1.6.6.2" --no-rdoc
+gem install mysql2 --version "~> 0.3.20" --no-rdoc
+
+gem install unicorn --version "~> 5.0.1" --no-rdoc
+gem install haml-rails --version "~> 0.9.0" --no-rdoc
 
 # Unversioned Gems
-gem install bundler haml-rails spring kaminari bullet guard guard-rspec guard-teaspoon \
-  letter_opener database_cleaner shoulda-matchers json_spec simplecov pg \
-  sqlite3 unicorn rubocop meta-tags pry pry-nav timecop
+gem install --no-rdoc bundler spring kaminari bullet guard guard-rspec guard-teaspoon \
+  database_cleaner shoulda-matchers json_spec simplecov pg sqlite3 rubocop meta-tags \
+  pry pry-nav timecop
