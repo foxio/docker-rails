ce7eaeb1e5908a515a052e1b25f1648f8377ef1d
Michael R. Fisher <mike@fox.io>
2016-04-13 08:02:58 -0400

    Cleanup startup scripts in image

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
 if [ -n "$CREATE_DATABASE" ]; then
+  echo "Creating the database..."
   bundle exec rake db:create
 fi
 
 if [ -n "$MIGRATE_DATABASE" ]; then
+  echo "Migrating the database..."
   bundle exec rake db:migrate
 fi
 
